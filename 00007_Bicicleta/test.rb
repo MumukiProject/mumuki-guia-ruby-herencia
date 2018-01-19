35353b7032e57c75939fe279c5829959421588f4
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-18 19:01:29 -0500

    Mumuki Export on 2018-01-18 19:01:29 -0500

--- 00007_Bicicleta/test.rb
+++ 00007_Bicicleta/test.rb
@@ -10,6 +10,10 @@ it "Una bicicleta inicializa con 0 kilómetros recorridos" do
   expect(bici.kilometros_recorridos).to eq 0
 end
 
+it "Una bicicleta tiene dos ruedas" do
+  expect(bici.cantidad_de_ruedas).to eq 2
+end
+
 it "Una bicicleta es ligera" do
   expect(auto.ligero?).to be true
 end
@@ -24,8 +28,4 @@ end
 it "Una bicicleta sigue siendo ligera después de conducirla muchos kilómetros" do
   bici.ligero?
   expect(bici.ligero?).to be true
-end
-
-it "Una bicicleta tiene dos ruedas" do
-  expect(bici.cantidad_de_ruedas).to eq 2
 end
