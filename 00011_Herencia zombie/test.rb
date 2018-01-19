b8c29c7e4bdd3740cfa0dbc58ed1639689e31180
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-18 19:53:23 -0500

    Mumuki Export on 2018-01-18 19:53:23 -0500

--- 00011_Herencia zombie/test.rb
+++ 00011_Herencia zombie/test.rb
@@ -1,4 +1,4 @@
-zombi = SuperZombi.new(100)
+superzombi = SuperZombi.new(100)
 zombi = Zombi.new(100)
 
 it "Si creo un nuevo Zombi le puedo especificar su salud inicial" do
@@ -36,30 +36,30 @@ it "Si creo un nuevo SuperZombi le puedo especificar su salud inicial" do
 end
 
 it "Un SuperZombi sabe correr" do
-  expect(zombi.sabe_correr?).to be true
+  expect(superzombi.sabe_correr?).to be true
 end
 
 it "Un SuperZombi grita ¡agrrrg!" do
-  expect(zombi.gritar).to eq "¡agrrrg!"
+  expect(superzombi.gritar).to eq "¡agrrrg!"
 end
 
 it "Un SuperZombi con salud inicial 100 no está sin vida" do
-  expect(zombi.sin_vida?).to eq false
+  expect(superzombi.sin_vida?).to eq false
 end
 
 it "Si un SuperZombi de salud 100 recibe 5 puntos de daño disminuye su salud en 15 puntos y no está sin vida" do
-  zombi.recibir_danio!(5)
-  expect(zombi.salud).to eq 85
-  expect(zombi.sin_vida?).to eq false
+  superzombi.recibir_danio!(5)
+  expect(superzombi.salud).to eq 85
+  expect(superzombi.sin_vida?).to eq false
 end
 
 it "Si un SuperZombi recibe mucho daño su salud es 0 y está sin vida" do
-  zombi.recibir_danio!(69)
-  expect(zombi.salud).to eq 0
-  expect(zombi.sin_vida?).to eq true
+  superzombi.recibir_danio!(69)
+  expect(superzombi.salud).to eq 0
+  expect(superzombi.sin_vida?).to eq true
 end
 
 it "Si un SuperZombi se regenera su salud vuelve a 100" do
-  zombi.regenerarse!
-  expect(zombi.salud).to eq 100
+  superzombi.regenerarse!
+  expect(superzombi.salud).to eq 100
 end
