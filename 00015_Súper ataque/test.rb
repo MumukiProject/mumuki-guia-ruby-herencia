af5f6a4b2cf5f478253c04987ce0aa0d395a72d5
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-19 08:59:12 -0500

    Mumuki Export on 2018-01-19 08:59:12 -0500

--- 00015_Súper ataque/test.rb
+++ 00015_Súper ataque/test.rb
@@ -13,13 +13,11 @@ class Zombi
 end
 
 bouba = Zombi.new
-kiki = Zombi.new
+kiki = Zombi.ne
+zombisexual = Zombi.new
 julian = Aliado.new
-anastasio = Aliado.new
 candelo = Aliado.new
 juliana = Sobreviviente.new
-anastasia = Sobreviviente.new
-candela = Sobreviviente.new
 persona = Persona.new
 
 it "La energía inicial de una sobreviviente es 1000" do
@@ -27,7 +25,7 @@ it "La energía inicial de una sobreviviente es 1000" do
 end
 
 it "Si una sobreviviente ataca normalmente su energía no se reduce" do
-  candela.atacar!(kiki, 5)
+  juliana.atacar!(kiki, 5)
   expect(candela.energia).to eq 1000
 end
 
@@ -56,7 +54,7 @@ it "Si un aliado bebe una bebida energética su energía aumenta un 10%" do
 end
 
 it "Si un aliado ataca con 5 puntos de daño a un zombi su salud disminuye en 10 puntos" do
-  julian.atacar!(bouba, 5)
+  julian.atacar!(zombisexual, 5)
   expect(bouba.salud).to eq 90
 end
 
